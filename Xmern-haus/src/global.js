3867f59ba7d13f873da490aea3c32c436df68384
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-05 11:09:14 -0600

    deployed to heroku

--- Xmern-haus/src/global.js
+++ Xmern-haus/src/global.js
@@ -1,8 +1,7 @@
 let BASE_URL = 'http://localhost:3001/'
 
-// if(process.env.NODE_ENV === 'production') {
-//     BASE_URL = 'https://bike-labs-4b05568e3735.herokuapp.com/'
-// }
+if(process.env.NODE_ENV === 'production') {
+    BASE_URL = 'https://mern-haus-380e6c20d7e0.herokuapp.com//'
+}
 
 export { BASE_URL }
-
