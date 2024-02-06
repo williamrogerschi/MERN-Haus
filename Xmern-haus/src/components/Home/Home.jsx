2e77071316575ba4762de49b14d6a0362f29ca68
williamrogerschi <william.rogers.chi@gmail.com>
2024-02-06 12:00:18 -0500

    fixing home page

--- Xmern-haus/src/components/Home/Home.jsx
+++ Xmern-haus/src/components/Home/Home.jsx
@@ -9,35 +9,7 @@ const Home = () => {
         <h1>Mern Pizza Haus</h1>
       </div>
       <div className="menu-container">
-        <div className='menu-link-container'>
-        <Link to='/Pizza' className="menu-item">
-          <div className="menu-text">
-            <h2>Menu</h2>
-            <p>Indulge in a symphony of flavors with our meticulously crafted pizza menu. From classic Margheritas to bold and innovative specialty pies, each slice is a celebration of quality ingredients, artisanal craftsmanship, and a passion for perfecting the art of pizza-making. Explore our diverse menu, where every topping, sauce, and crust is a carefully curated delight, promising an unforgettable culinary journey with every bite. Elevate your pizza experience with our signature creations that redefine traditional favorites or savor the familiarity of time-tested classics. Welcome to a world where passion meets palate, and each pizza is a masterpiece waiting to be savored.</p>
-          </div>
-          <div className="menu-image">
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/5f0a575f2b3bb20c7584bc42/1675659810793-BBGKD1MQMFFMWX55IS3B/PizzaPasta_LeighGriffiths-39+hero-banner.jpg?format=2500w"
-              alt="Pizza"
-              className="circular-image"
-            />
-          </div>
-        </Link>
-        <Link to='/CYOP' className="menu-item">
-          <div className="menu-image">
-            <img
-              src="https://cdn.tgdd.vn/2021/09/CookDish/cach-bao-quan-pizza-de-banh-pizza-va-cach-ham-nong-banh-pizza-avt-1200x676.jpg"
-              alt="Pizza"
-              className="circular-image"
-            />
-          </div>
-          <div className="menu-text">
-            <h2>Create Your Own Pizza - CYOP</h2>
-            <p>Craft your perfect pizza with our Create Your Own option! Start with fresh dough, choose from a variety of premium sauces, and customize with your favorite toppings. Whether you prefer classic or adventurous, thin or thick crust, the power to create is yours. At Mern Pizza Haus, every pizza is a unique masterpiece reflecting your taste and style.</p>
-          </div>
-        </Link>
-        </div>
-          <div className="about-us-container">
+      <div className="about-us-container">
             <div className="about-us-text">
               <h2>About Us</h2>
               <p>Welcome to Mern Pizza Haus, where passion for pizza meets culinary excellence. Our story began in the heart of Italy, where our founder, Antonio, learned the art of crafting perfect pizza from his grandmother's secret recipe.
@@ -54,6 +26,35 @@ Grazie mille for choosing us!</p>
               <img src="/lamp.jpg" alt="Mern Pizza Haus" />
             </div>
           </div>
+        <div className='menu-link-container'>
+        <Link to='/Pizza' className="menu-item">
+        <div className="menu-image">
+            <img
+              src="https://images.squarespace-cdn.com/content/v1/5f0a575f2b3bb20c7584bc42/1675659810793-BBGKD1MQMFFMWX55IS3B/PizzaPasta_LeighGriffiths-39+hero-banner.jpg?format=2500w"
+              alt="Pizza"
+              className="circular-image"
+            />
+          </div>
+          <div className="menu-text">
+            <h2>Menu</h2>
+            <p>Savor a symphony of flavors at Mern Pizza Haus, where every slice is a celebration of quality ingredients and artisanal craftsmanship. From classic Margheritas to innovative specialty pies, our diverse menu promises an unforgettable culinary journey.</p>
+          </div>
+
+        </Link>
+        <Link to='/CYOP' className="menu-item">
+          <div className="menu-image">
+            <img
+              src="https://cdn.tgdd.vn/2021/09/CookDish/cach-bao-quan-pizza-de-banh-pizza-va-cach-ham-nong-banh-pizza-avt-1200x676.jpg"
+              alt="Pizza"
+              className="circular-image"
+            />
+          </div>
+          <div className="menu-text">
+            <h2>Create Your Own Pizza - CYOP</h2>
+            <p>Unleash your creativity at Mern Pizza Haus with our Create Your Own option! Start with fresh dough, choose premium sauces, and customize with favorite toppings. Whether you crave classic or adventurous, thin or thick crust, the power to create your unique masterpiece is in your hands.</p>
+          </div>
+        </Link>
+        </div>
       </div>
     </div>
   )
