588ce065d7e0d813511f3d90267f8f6386c87670
Billy Rogers <william.rogers.chi@gmail.com>
2026-02-09 10:51:45 -0500

    updating mongodb

--- db/index.js
+++ db/index.js
@@ -16,8 +16,9 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const dbName = 'mernHausDatabase'
-    const uri = `mongodb+srv://williamrogerschi:Jukath00n1990!@pizzahaus.uynbwsf.mongodb.net/${dbName}?retryWrites=true&w=majority`
-
+    // const uri = `mongodb+srv://williamrogerschi:Jukath00n1990!@pizzahaus.uynbwsf.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    const uri = "mongodb+srv://williamrogerschi:Jukath00n1990!@pizzahaus.uynbwsf.mongodb.net/?appName=PizzaHaus"
+    
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
